23b114f939767a83ac7d6b9551ae725e81635114
Aditi Lal <139861603+lal15aditi@users.noreply.github.com>
2024-03-23 20:16:11 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,13 @@
-
 import React from "react";
-import './../styles/App.css';
+import "./../styles/App.css";
+import UserInfo from "./UserInfo";
 
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+      <UserInfo />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
